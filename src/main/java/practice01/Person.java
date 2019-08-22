119f6cf110c3abbbd48fb9924d53c3c3ad98f9c6
LZW <1312299240@qq.com>
2019-08-22 22:13:02 +0800

    1

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,29 @@
 package practice01;
 
 public class Person {
+	private String name;
+	protected Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	private int age;
+	
+	public String introduce() {
+		return String.format("My name is %s. I am %d years old.", this.name, this.age);
+	}
+	
+	
 }
